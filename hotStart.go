8c11c3935573d2331ccf1641c69d531144f086b9
=baichonghua <18211167516@163.com>
2020-08-31 15:10:31 +0800

    d

--- hotStart.go
+++ hotStart.go
@@ -10,53 +10,69 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 )
 
 const (
-	GRACEFUL_ENVIRON_KEY    = "IS_GRACEFUL"
-	GRACEFUL_ENVIRON_STRING = GRACEFUL_ENVIRON_KEY + "=1"
-	GRACEFUL_LISTENER_FD    = 3
-	DEFAULT_READ_TIMEOUT    = 60 * time.Second
-	DEFAULT_WRITE_TIMEOUT   = DEFAULT_READ_TIMEOUT
+	LISTENER_FD           = 3
+	DEFAULT_READ_TIMEOUT  = 60 * time.Second
+	DEFAULT_WRITE_TIMEOUT = DEFAULT_READ_TIMEOUT
 )
 
-// HTTP server that supported graceful shutdown or restart
-type Server struct {
+var (
+	runMutex = sync.RWMutex{}
+)
+
+// HTTP server that supported hotstart shutdown or restart
+type HotServer struct {
 	*http.Server
 	listener     net.Listener
-	isGraceful   bool
+	isChild      bool
 	signalChan   chan os.Signal
 	shutdownChan chan bool
+	BeforeBegin  func(addr string)
 }
 
 func ListenAndServe(addr string, handler http.Handler) error {
 	return NewServer(addr, handler, DEFAULT_READ_TIMEOUT, DEFAULT_WRITE_TIMEOUT).ListenAndServe()
 }
 
-func NewServer(addr string, handler http.Handler, readTimeout, writeTimeout time.Duration) *Server {
-	isGraceful := false
-	if os.Getenv(GRACEFUL_ENVIRON_KEY) != "" {
-		isGraceful = true
-	}
+/*
+new HotServer
+*/
+func NewServer(addr string, handler http.Handler, readTimeout, writeTimeout time.Duration) (srv *HotServer) {
+	runMutex.Lock()
+	defer runMutex.Unlock()
 
-	return &Server{
+	isChild = os.Getenv("HOT_CONTINUE") != ""
+
+	srv = Server{
 		Server: &http.Server{
-			Addr:    addr,
-			Handler: handler,
-
+			Addr:         addr,
+			Handler:      handler,
 			ReadTimeout:  readTimeout,
 			WriteTimeout: writeTimeout,
 		},
 
-		isGraceful:   isGraceful,
+		isChild:      isChild,
 		signalChan:   make(chan os.Signal),
 		shutdownChan: make(chan bool),
 	}
+
+	//服务启动之前钩子，命令行输出pid
+	srv.BeforeBegin = func(addr string) {
+		srv.logf(addr)
+	}
+
+	return
 }
 
-func (srv *Server) ListenAndServe() error {
+/*
+Listen http server
+*/
+func (srv *HotServer) ListenAndServe() error {
 	addr := srv.Addr
 	if addr == "" {
 		addr = ":http"
@@ -68,9 +84,20 @@ func (srv *Server) ListenAndServe() error {
 	}
 
 	srv.listener = ln
+
+	if srv.isChild {
+		//通知父进程不接受请求
+		syscall.Kill(syscall.Getppid(), syscall.SIGTERM)
+	}
+
+	srv.BeforeBegin(srv.Addr)
+
 	return srv.Serve()
 }
 
+/*
+监听 https server
+*/
 func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
 	addr := srv.Addr
 	if addr == "" {
@@ -98,10 +125,21 @@ func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
 	}
 
 	srv.listener = tls.NewListener(ln, config)
+
+	if srv.isChild {
+		syscall.Kill(syscall.Getppid(), syscall.SIGTERM)
+	}
+
+	srv.BeforeBegin(srv.Addr)
+
 	return srv.Serve()
 }
 
+/*
+服务启动
+*/
 func (srv *Server) Serve() error {
+	//监听信号
 	go srv.handleSignals()
 	err := srv.Server.Serve(srv.listener)
 
@@ -113,12 +151,12 @@ func (srv *Server) Serve() error {
 	return err
 }
 
-func (srv *Server) getNetListener(addr string) (net.Listener, error) {
-	var ln net.Listener
-	var err error
-
-	if srv.isGraceful {
-		file := os.NewFile(GRACEFUL_LISTENER_FD, "")
+/*
+get lister
+*/
+func (srv *Server) getNetListener(addr string) (ln net.Listener, err error) {
+	if srv.isChild {
+		file := os.NewFile(LISTENER_FD, "")
 		ln, err = net.FileListener(file)
 		if err != nil {
 			err = fmt.Errorf("net.FileListener error: %v", err)
@@ -134,6 +172,10 @@ func (srv *Server) getNetListener(addr string) (net.Listener, error) {
 	return ln, nil
 }
 
+/*
+监听信号
+*/
+
 func (srv *Server) handleSignals() {
 	var sig os.Signal
 
@@ -147,23 +189,22 @@ func (srv *Server) handleSignals() {
 		sig = <-srv.signalChan
 		switch sig {
 		case syscall.SIGTERM:
-			srv.logf("received SIGTERM, graceful shutting down HTTP server.")
-			srv.shutdownHTTPServer()
+			srv.logf("received SIGTERM, hotstart shutting down HTTP server.")
+			srv.shutdown()
 		case syscall.SIGUSR2:
-			srv.logf("received SIGUSR2, graceful restarting HTTP server.")
-
-			if pid, err := srv.startNewProcess(); err != nil {
-				srv.logf("start new process failed: %v, continue serving.", err)
-			} else {
-				srv.logf("start new process successed, the new pid is %d.", pid)
-				srv.shutdownHTTPServer()
+			srv.logf("received SIGUSR2, hotstart restarting HTTP server.")
+			if err := srv.fork(); err != nil {
+				log.Println("Fork err:", err)
 			}
 		default:
 		}
 	}
 }
 
-func (srv *Server) shutdownHTTPServer() {
+/*
+优雅关闭后台
+*/
+func (srv *Server) shutdown() {
 	if err := srv.Shutdown(context.Background()); err != nil {
 		srv.logf("HTTP server shutdown error: %v", err)
 	} else {
@@ -173,49 +214,48 @@ func (srv *Server) shutdownHTTPServer() {
 }
 
 // start new process to handle HTTP Connection
-func (srv *Server) startNewProcess() (uintptr, error) {
-	listenerFd, err := srv.getTCPListenerFd()
+func (srv *Server) fork() error {
+	listener, err := srv.getTCPListenerFile()
 	if err != nil {
-		return 0, fmt.Errorf("failed to get socket file descriptor: %v", err)
+		return fmt.Errorf("failed to get socket file descriptor: %v", err)
 	}
 
-	// set graceful restart env flag
-	envs := []string{}
-	for _, value := range os.Environ() {
-		if value != GRACEFUL_ENVIRON_STRING {
-			envs = append(envs, value)
-		}
-	}
-	envs = append(envs, GRACEFUL_ENVIRON_STRING)
+	// set hotstart restart env flag
+	env := append(
+		os.Environ(),
+		"HOT_CONTINUE=1",
+	)
 
 	execSpec := &syscall.ProcAttr{
-		Env:   envs,
-		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd(), listenerFd},
+		Env:   env,
+		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd(), listener.Fd()},
 	}
 
 	fork, err := syscall.ForkExec(os.Args[0], os.Args, execSpec)
 	if err != nil {
-		return 0, fmt.Errorf("failed to forkexec: %v", err)
+		return fmt.Errorf("Restart: Failed to launch, error: %v", err)
 	}
 
-	return uintptr(fork), nil
+	return nil
 }
 
-func (srv *Server) getTCPListenerFd() (uintptr, error) {
+/*
+获取TCP监听文件
+*/
+func (srv *Server) getTCPListenerFile() (*os.File, error) {
 	file, err := srv.listener.(*net.TCPListener).File()
 	if err != nil {
 		return 0, err
 	}
-	return file.Fd(), nil
+	return file, nil
 }
+
+/*
+格式化输出Log
+*/
 
 func (srv *Server) logf(format string, args ...interface{}) {
 	pids := strconv.Itoa(os.Getpid())
 	format = "[pid " + pids + "] " + format
-
-	if srv.ErrorLog != nil {
-		srv.ErrorLog.Printf(format, args...)
-	} else {
-		log.Printf(format, args...)
-	}
+	log.Printf(format, args...)
 }
